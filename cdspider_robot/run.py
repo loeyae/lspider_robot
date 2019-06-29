ce595fde2a9e5d93673d083ba5d0adb6fa420d00
loeyae@gmail.com <zyq834100>
2019-06-29 20:44:56 +0800

    fix bug

--- cdspider_robot/run.py
+++ cdspider_robot/run.py
@@ -12,17 +12,17 @@ from cdspider.run import *
 rdir = os.path.dirname(os.path.abspath(__file__))
 
 @cli.command()
-@click.option('--rebot-cls', default='cdspider_robot.robots.WxchatRobots', callback=load_cls, help='schedule name', show_default=True)
+@click.option('--robot-cls', default='cdspider_robot.robots.WxchatRobots', callback=load_cls, help='schedule name', show_default=True)
 @click.option('--aichat-rpc', default='http://127.0.0.1:27777', help='robot rpc server', show_default=True)
 @click.option('-u', '--uuid', help='唯一标识')
 @click.pass_context
-def wechat(ctx, rebot_cls, aichat_rpc, uuid):
+def wechat(ctx, robot_cls, aichat_rpc, uuid):
     """
     web wechat
     """
     aichat_rpc = connect_rpc(ctx, None, aichat_rpc)
-    rebot_cls = load_cls(ctx, None, rebot_cls)
-    robot = rebot_cls(ctx, uuid=uuid)
+    robot_cls = load_cls(ctx, None, robot_cls)
+    robot = robot_cls(ctx, uuid=uuid)
     reply = lambda m, s: aichat_rpc.reply(m, s)
     def init_aichat(m, s):
         info = m.search_friends()
@@ -38,21 +38,21 @@ def wechat(ctx, rebot_cls, aichat_rpc, uuid):
     robot.run()
 
 @cli.command()
-@click.option('--rebot-cls', default='cdspider_robot.robots.AichatRobots', callback=load_cls, help='schedule name', show_default=True)
+@click.option('--robot-cls', default='cdspider_robot.robots.AichatRobots', callback=load_cls, help='schedule name', show_default=True)
 @click.option('-u', '--uuid', default=None, help='唯一标识', show_default=True)
 @click.option('-b', '--bot-data', default=os.path.join(rdir, 'config', 'aiml'), help='AI头脑文件目录', show_default=True)
 @click.option('-c', '--commands', multiple=True, help='commands')
 @click.pass_context
-def aichat(ctx, rebot_cls, uuid, bot_data, commands):
+def aichat(ctx, robot_cls, uuid, bot_data, commands):
     """
     Aiml bot
     """
-    rebot_cls = load_cls(ctx, None, rebot_cls)
-    robot = rebot_cls(ctx, commands = commands, bot_data = bot_data)
+    robot_cls = load_cls(ctx, None, robot_cls)
+    robot = robot_cls(ctx, commands = commands, bot_data = bot_data)
     robot.run(uuid)
 
 @cli.command()
-@click.option('--rebot-cls', default='cdspider_robot.robots.AichatRobots', callback=load_cls, help='rebot name', show_default=True)
+@click.option('--robot-cls', default='cdspider_robot.robots.AichatRobots', callback=load_cls, help='robot name', show_default=True)
 @click.option('-b', '--bot-data', default=os.path.join(rdir, 'config', 'aiml'), help='AI头脑文件目录', show_default=True)
 @click.option('-c', '--commands', multiple=True, help='commands')
 @click.option('-s', '--settings', default=None, multiple=True, help='bot settings: [name, sex, age, company]', show_default=True)
@@ -60,12 +60,12 @@ def aichat(ctx, rebot_cls, uuid, bot_data, commands):
 @click.option('--xmlrpc-port', default=27777, help="xmlrpc bind port", show_default=True)
 @click.option('--debug', default=False, is_flag=True, help='debug模式', show_default=True)
 @click.pass_context
-def aichat_rpc(ctx, rebot_cls, bot_data, commands, settings, xmlrpc_host, xmlrpc_port, debug):
+def aichat_rpc(ctx, robot_cls, bot_data, commands, settings, xmlrpc_host, xmlrpc_port, debug):
     """
     Aiml bot rpc
     """
-    rebot_cls = load_cls(ctx, None, rebot_cls)
-    robot = rebot_cls(ctx, commands = commands, bot_data = bot_data, settings = settings)
+    robot_cls = load_cls(ctx, None, robot_cls)
+    robot = robot_cls(ctx, commands = commands, bot_data = bot_data, settings = settings)
     robot.xmlrpc_run(xmlrpc_port, xmlrpc_host)
 
 @cli.command()
