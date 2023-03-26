37eeb7684b03da955d4f49ebc16011d709650183
Zhangyi <loeyae@gmail.com>
2023-03-26 12:58:08 +0800

    更新依赖

--- setup.py
+++ setup.py
@@ -1,29 +1,28 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # Licensed under the Apache License, Version 2.0 (the "License"),
 # see LICENSE for more details: http://www.apache.org/licenses/LICENSE-2.0.
 
-__author__="Zhang Yi <loeyae@gmail.com>"
-__date__ ="$2019-2-19 9:16:07$"
-
+__author__ = "Zhang Yi <loeyae@gmail.com>"
+__date__ = "$2019-2-19 9:16:07$"
 
 from setuptools import setup, find_packages
 
 setup(
-    name = "cdspider_robot",
-    version = "0.1",
-    description = "数据采集框架聊天机器人",
-    author = 'Zhang Yi',
-    author_email = 'loeyae@gmail.com',
-    license = "Apache License, Version 2.0",
+    name="cdspider_robot",
+    version="0.1.1",
+    description="数据采集框架聊天机器人",
+    author='Zhang Yi',
+    author_email='loeyae@gmail.com',
+    license="Apache License, Version 2.0",
     url="https://github.com/loeyae/lspider_robot.git",
-    install_requires = [
+    install_requires=[
         'aiml>=0.9.1'
         'itchat>=1.3.10',
-        'cdspider>=0.1',
+        'cdspider>=0.1.4',
     ],
-    packages = find_packages(),
+    packages=find_packages(),
 
-    package_data = {
+    package_data={
         'cdspider_robot': [
             'config/*.conf',
             'config/*.json',
@@ -31,7 +30,7 @@ setup(
             'config/aiml/*.xml',
         ]
     },
-    entry_points = {
+    entry_points={
         'console_scripts': [
             'cdspider_robot = cdspider_robot.run:main',
         ],
